3d783a0a4ff53700fd1ae19be4997a289c5bbf64
Max Glasmacher <maxglasmacher@gmail.com>
2019-02-18 17:49:13 +0100

    photouploader on user and yacht model done

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -2,4 +2,6 @@ class Yacht < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :previous_users, through: :bookings, source: :user
+
+  mount_uploader :photo, PhotoUploader
 end
