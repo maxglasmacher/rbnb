92577e922df39360c1bf5f3c94acc60df23a1cd7
Max Glasmacher <maxglasmacher@gmail.com>
2019-02-19 18:03:39 +0100

    minor dash change

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :yachts do
     resources :bookings, except: [ :show, :index ]
   end
+  get 'yachts/:id/users', to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
