599805ca1cdfa601cbd75eb4ca8137bd5c0aaf5a
Max Glasmacher <maxglasmacher@gmail.com>
2019-02-20 15:55:07 +0100

    add flatpickr successfully to showpage

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,6 +1,6 @@
 class Yacht < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :previous_users, through: :bookings, source: :user
 
   mount_uploader :photo, PhotoUploader
